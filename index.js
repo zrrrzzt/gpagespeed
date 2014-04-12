87ed2825102815a51db204056c63d7cf9abb742d
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-04-12 20:57:57 +0200

    Switches to requests qs. Removes util dependency

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var request = require('request')
-  , util = require('util')
   , pagespeedUrl = 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed';
 
 module.exports = function(opts, callback){
@@ -10,8 +9,7 @@ module.exports = function(opts, callback){
     return callback(new Error('Missing required param: url'), null);
   }
 
-  var uri = util.format('%s?url=%s&key=%s', pagespeedUrl, opts.url, opts.key);
-  request(uri, function(error, res, body){
+  request(pagespeedUrl,{qs:opts}, function(error, res, body){
     if(error){
       return callback(error, null);
     }
