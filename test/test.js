3d5a30ca02663a52f951be01a9f8a867282c9bef
Geir Gåsodden <geir.gasodden@pythonia.no>
2014-05-30 15:48:40 +0200

    Removes duplicate test

--- test/test.js
+++ test/test.js
@@ -1,19 +1,9 @@
 var pagespeed = require('../index')
   , assert = require('assert')
   , url = 'http://www.google.com'
-  , key = 'insert-your-API-key';
+  ;
 
 describe('PageSpeed', function(){
-
-  it('Should throw error if API key is not supplied', function(done){
-    var opts = {'url':url};
-    pagespeed(opts, function(err, data){
-      assert.throws(function(){
-        if(err) throw err;
-      });
-      done();
-    });
-  });
 
   it('Should return data as JSON', function(done){
     var opts = {nokey:true, url:url};
