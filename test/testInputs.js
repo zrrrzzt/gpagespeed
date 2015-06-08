91c64f401b7069b8d061e2fafe582f29a974a085
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-06-08 21:30:10 +0200

    Tries to please jscs

--- test/testInputs.js
+++ test/testInputs.js
@@ -1,74 +1,82 @@
 'use strict';
 
-var assert = require('assert')
-  , gps = require('../index')
-  ;
+var assert = require('assert');
+var gps = require('../index');
 
-describe('gpagespeed - inputs', function(){
+describe('gpagespeed - inputs', function() {
 
-  it('Should throw if url is not specified', function(done){
+  it('Should throw if url is not specified', function(done) {
 
     var opts = {
-        url:false,
-        key:true
-      }
-      ;
+      url:false,
+      key:true
+    };
 
-    gps(opts, function(err, data){
-      assert.throws(function(){
-          if(err) throw err;
-        }, function(err){
-          if((err instanceof Error) && /Missing required param: url/.test(err)){
+    gps(opts, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required param: url/.test(err)) {
             return true
           }
         },
-        "Unexpected error"
+        'Unexpected error'
       );
       done();
     });
 
   });
 
-  it('Should throw if url is not valid', function(done){
+  it('Should throw if url is not valid', function(done) {
 
     var opts = {
-        url:'pysje',
-        key:true
-      }
-      ;
+      url:'pysje',
+      key:true
+    };
 
-    gps(opts, function(err, data){
-      assert.throws(function(){
-          if(err) throw err;
-        }, function(err){
-          if((err instanceof Error) && /Invalid url/.test(err)){
+    gps(opts, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Invalid url/.test(err)) {
             return true
           }
         },
-        "Unexpected error"
+        'Unexpected error'
       );
       done();
     });
 
   });
 
-  it('Should throw if key is not supplied', function(done){
+  it('Should throw if key is not supplied', function(done) {
 
     var opts = {
-        url:'https://www.npmjs.org',
-        key:false
-      }
-      ;
+      url:'https://www.npmjs.org',
+      key:false
+    };
 
-    gps(opts, function(err, data){
-      assert.throws(function(){
-          if(err) throw err;
-        }, function(err){
-          if((err instanceof Error) && /Missing required param: key/.test(err)){
+    gps(opts, function(err, data) {
+      assert.throws(function() {
+          if (err) {
+            throw err;
+          } else {
+            console.log(data);
+          }
+        }, function(err) {
+          if ((err instanceof Error) && /Missing required param: key/.test(err)) {
             return true
           }
         },
-        "Unexpected error"
+        'Unexpected error'
       );
       done();
     });
