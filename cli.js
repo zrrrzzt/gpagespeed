8b7f157efe0ea9c0abb64b6e7e3500da5cbb2a17
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-06-08 20:54:57 +0200

    Makes jscs happy

--- cli.js
+++ cli.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 'use strict';
 
-var pagespeed = require('./index')
-  , pkg = require('./package.json')
-  , query = process.argv[2]
-  , argv = require('minimist')((process.argv.slice(2)))
-  , opts = {}
-  ;
+var pagespeed = require('./index');
+var pkg = require('./package.json');
+var query = process.argv[2];
+var argv = require('minimist')((process.argv.slice(2)));
+var opts = {};
 
 function printHelp() {
   console.log(pkg.description);
@@ -15,7 +14,7 @@ function printHelp() {
   console.log('  $ gpagespeed <url> --key=<key>');
   console.log('');
   console.log('Optional, supply other arguments.');
-  console.log('See https://developers.google.com/speed/docs/insights/v1/getting_started for description');
+  console.log('See https://developers.google.com/speed/docs/insights/v1/getting_started');
   console.log('  $ gpagespeed <url> --key=<key> --callback=<callback> --prettyprint=<true> --userIp=<userIp> --locale=<locale> --strategy=<desktop|mobile>');
   console.log('You can also use request instead of googleapis and pick another version of the api');
   console.log('  $ gpagespeed <url> --key=<key> --userequest=true --apiversion=<versionstring>');
@@ -35,53 +34,53 @@ if (query.indexOf('http') !== -1) {
   opts.url = argv._[0];
 }
 
-if(!opts.url || (!argv.key && !argv.nokey)){
+if (!opts.url || (!argv.key && !argv.nokey)) {
   printHelp();
   return;
 }
 
-if(argv.url){
+if (argv.url) {
   opts.url = argv.url;
 }
 
-if(argv.key){
+if (argv.key) {
   opts.key = argv.key;
 }
 
-if(argv.callback){
+if (argv.callback) {
   opts.callback = argv.callback;
 }
 
-if(argv.prettyprint){
+if (argv.prettyprint) {
   opts.prettyprint = argv.prettyprint;
 }
 
-if(argv.userIp){
+if (argv.userIp) {
   opts.userIp = argv.userIp;
 }
 
-if(argv.locale){
+if (argv.locale) {
   opts.locale = argv.locale;
 }
 
-if(argv.strategy){
+if (argv.strategy) {
   opts.strategy = argv.strategy;
 }
 
-if(argv.nokey){
+if (argv.nokey) {
   opts.nokey = argv.nokey;
 }
 
-if(argv.userequest){
+if (argv.userequest) {
   opts.userequest = argv.userequest;
 }
 
-if(argv.apiversion){
+if (argv.apiversion) {
   opts.apiversion = argv.apiversion;
 }
 
-pagespeed(opts, function(err, data){
-  if(err){
+pagespeed(opts, function(err, data) {
+  if (err) {
     console.log(err);
   } else {
     console.log(data);
--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-var google = require('googleapis')
-  , request = require('request')
-  , validUrl = require('valid-url')
-  , apiVersion
-  , pagespeedUrl
-  , pagespeedonline
-  ;
+var google = require('googleapis');
+var request = require('request');
+var validUrl = require('valid-url');
+var apiVersion;
+var pagespeedUrl;
+var pagespeedonline;
 
-module.exports = function(opts, callback){
+module.exports = function(options, callback) {
 
-  if(!opts.key && !opts.nokey){
+  if (!options.key && !options.nokey) {
     return callback(new Error('Missing required param: key'), null);
   }
 
-  if(!opts.url){
+  if (!options.url) {
     return callback(new Error('Missing required param: url'), null);
   }
 
-  if(opts.url && !validUrl.isWebUri(opts.url)){
+  if (options.url && !validUrl.isWebUri(options.url)) {
     return callback(new Error('Invalid url'), null);
   }
 
-  apiVersion = opts.apiversion || 'v2';
+  apiVersion = options.apiversion || 'v2';
 
-  if(opts.userequest){
+  if (options.userequest) {
     pagespeedUrl = 'https://www.googleapis.com/pagespeedonline/' + apiVersion + '/runPagespeed';
-    request(pagespeedUrl, {qs:opts}, function(error, response, body){
-      if(error){
+    request(pagespeedUrl, {qs:options}, function(error, response, body) {
+      if (error) {
         return callback(error, null);
       } else {
         return callback(null, body.toString());
@@ -33,8 +32,8 @@ module.exports = function(opts, callback){
     })
   } else {
     pagespeedonline = google.pagespeedonline(apiVersion);
-    pagespeedonline.pagespeedapi.runpagespeed(opts, function(error, req){
-      if(error){
+    pagespeedonline.pagespeedapi.runpagespeed(options, function(error, req) {
+      if (error) {
         return callback(error, null);
       }
       return callback(null, req);
