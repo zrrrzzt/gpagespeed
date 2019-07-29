51c2be809c1f2aeeef67eec97fda6a9e6fe0a8a1
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-07-29 07:05:29 +0200

    Updates to pass latest linter (patch)

--- index.js
+++ index.js
@@ -2,41 +2,31 @@ const { google } = require('googleapis')
 const validUrl = require('valid-url')
 const getResults = require('./lib/get-result')
 
-module.exports = options => {
-  return new Promise(async (resolve, reject) => {
-    if (!options.key && !options.nokey) {
-      const error = new Error('Missing required param: key')
-      return reject(error)
-    }
+module.exports = async options => {
+  if (!options.key && !options.nokey) {
+    const error = new Error('Missing required param: key')
+    throw error
+  }
 
-    if (!options.url) {
-      const error = new Error('Missing required param: url')
-      return reject(error)
-    }
+  if (!options.url) {
+    const error = new Error('Missing required param: url')
+    throw error
+  }
 
-    if (options.url && !validUrl.isWebUri(options.url)) {
-      const error = new Error('Invalid url')
-      return reject(error)
-    }
+  if (options.url && !validUrl.isWebUri(options.url)) {
+    const error = new Error('Invalid url')
+    throw error
+  }
 
-    const apiVersion = options.apiversion || 'v4'
+  const apiVersion = options.apiversion || 'v4'
 
-    if (options.useweb) {
-      const pagespeedUrl = `https://www.googleapis.com/pagespeedonline/${apiVersion}/runPagespeed`
-      try {
-        const data = await getResults({ apiUrl: pagespeedUrl, qs: options })
-        return resolve(data)
-      } catch (error) {
-        return reject(error)
-      }
-    } else {
-      const pagespeedonline = google.pagespeedonline(apiVersion)
-      try {
-        const { data } = await pagespeedonline.pagespeedapi.runpagespeed(options)
-        return resolve(data)
-      } catch (error) {
-        return reject(error)
-      }
-    }
-  })
+  if (options.useweb) {
+    const pagespeedUrl = `https://www.googleapis.com/pagespeedonline/${apiVersion}/runPagespeed`
+    const data = await getResults({ apiUrl: pagespeedUrl, qs: options })
+    return data
+  } else {
+    const pagespeedonline = google.pagespeedonline(apiVersion)
+    const { data } = await pagespeedonline.pagespeedapi.runpagespeed(options)
+    return data
+  }
 }
